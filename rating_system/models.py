ce94e5fae432beb07635cd9ef8fdceebc7ca3af2
Bo Hou <ml18b6h@leeds.uk>
2020-03-13 04:18:27 +0000

    1

--- rating_system/models.py
+++ rating_system/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Professors(models.Model):
+    id = models.AutoField(primary_key=True)
     professor_id = models.CharField(max_length=10, unique=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -9,6 +10,7 @@ class Professors(models.Model):
         return u'%s, %s, %s' % (self.professor_id, self.first_name,self.last_name)
 
 class Moudles(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=10)
     year =models.CharField(max_length=4)
@@ -25,7 +27,7 @@ class Rating(models.Model):
     stars = models.IntegerField()
     module = models.ForeignKey(Moudles, on_delete=models.CASCADE)
     def __str__(self):
-        return u'%s, %s, %s' % (self.professer_id, self.stars, self.module.code)
+        return str(self.stars)
 class Users(models.Model):
     userName = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=10)
--- rating_system/views.py
+++ rating_system/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rating_system import models
 from decimal import *
 import json
@@ -7,79 +8,122 @@ import json
 def index(request):
     return HttpResponse("Hello, world. You're at the rating_system index.")
 def professer_id_list(self):
+    print(self.taughtBy.all())
     return ','.join([i.professor_id for i in self.taughtBy.all()])
 def showMoudles(request):
     # if request.session.get("userName")== None:
     #     return HttpResponse("please login or register first")
     # print(request.session.get("userName"))
+    user_list = list(models.Users.objects.all())
+    active_list = list()
+    for item in user_list:
+        if item.is_active == '1':
+            active_list.append(item.userName)
+            active_list.append('1')
+    if '1' not in active_list:
+        return HttpResponse("please login or register first")
     modules_list = models.Moudles.objects.all()
     professors = models.Professors.objects.all()
     outList = list()
     professorsList = list()
+    temp_list = list()
     for e in modules_list:
-        # print(e)
-        # print(professer_id_list(e))
-        for id in professer_id_list(e).split(","):
-            professorsList.append(professors.get(professor_id=id).__str__())
-        dict = {"Code":e.code, "Name":e.name, "Year":e.year, "Semester":e.semester,"Taught by":professorsList}
-        outList.append(dict)
+        professorsList.clear()
+        for p in e.taughtBy.all():
+            professorsList.append(p.__str__())
+        # print(professorsList)
+        # dict = {"Code":e.code, "Name":e.name, "Year":e.year, "Semester":e.semester,"Taught by":professorsList.__str__()}
+        out_str = "Module_Code:"+e.code+"  Name:"+e.name+" Year:"+e.year+" Semester:" +e.semester," Taught by:" + professorsList.__str__()
+        outList.append(out_str)
+        outList.append("\n")
+    print(outList)
+
+    # for e in modules_list:
+    #     for id in professer_id_list(e).split(","):
+    #         professorsList.append(professors.get(professor_id=id).__str__())
+    #     dict = {"Code":e.code, "Name":e.name, "Year":e.year, "Semester":e.semester,"Taught by":professorsList}
+    #     outList.append(dict)
     return HttpResponse(json.dumps(outList))
 
 def showRatings(request):
 #     if request.session.get("userName")== None:
 #         return HttpResponse("please login or register first")
+    user_list = list(models.Users.objects.all())
+    active_list = list()
+    for item in user_list:
+        if item.is_active == '1':
+            active_list.append(item.userName)
+            active_list.append('1')
+    if '1' not in active_list:
+        return HttpResponse("please login or register first")
     professors = models.Professors.objects.all()
-    ratings = models.Rating.objects.all()
-    ratings_list =list(ratings.values_list('professer_id'))
-    temp_rating_list=list()
-    cal_rating_list = list()
-    for id in ratings_list:
-        if id not in temp_rating_list:
-            temp_rating_list.append(id)
-    for id in temp_rating_list:
-        cal_rating_list.append(models.Rating.objects.filter(professer_id=id))
+    print(professors)
+    id_list = list()
     cal_list = list()
-    for items in cal_rating_list:
-        total_star = 1
-        number = 0;
-        p_id = 0;
-        for idx,item in enumerate(items):
-            total_star+=int(item.stars)
-            number = idx+1
-            p_id = item.professer_id
-        p_list = list(models.Professors.objects.filter(id=p_id))
+    for p in professors:
+        total_star = 0
+        number = 0
+        ratings  = models.Rating.objects.filter(professer_id=p.id)
+        for item in ratings:
+            total_star += int(item.stars)
+            number += 1
+        print(total_star,number)
         avg_star = str(int(Decimal(total_star / number).quantize(Decimal('1'), rounding=ROUND_HALF_UP))*'*')
-        cal_list.append("The rating of Professpr " + p_list[0].first_name + "." + p_list[0].last_name + " (" + p_list[0].professor_id + ") is:" + avg_star)
+        cal_list.append("The rating of Professor " + p.first_name + "." + p.last_name + " (" + p.professor_id + ") is:" + avg_star)
+        cal_list.append("\n")
     return HttpResponse(cal_list)
 def showRatings4Moudles(request):
     # if request.session.get("userName")== None:
     #     return HttpResponse("please login or register first")
+    user_list = list(models.Users.objects.all())
+    active_list = list()
+    for item in user_list:
+        if item.is_active == '1':
+            active_list.append(item.userName)
+            active_list.append('1')
+    if '1' not in active_list:
+        return HttpResponse("please login or register first")
     professor_id = request.GET.get('professor_id',None)
     module_code = request.GET.get('module_code',None)
-    print(professor_id,module_code)
     if professor_id ==None or module_code==None:
         return HttpResponse("please make sure all input params are not null")
     try:
-        ratings = list(models.Rating.objects.filter(professer__professor_id=professor_id,professer__moudles__code=module_code))
-        print(ratings)
+        total_star = 0
+        number = 0
         p = list(models.Professors.objects.filter(professor_id=professor_id))
         m = list(models.Moudles.objects.filter(code=module_code))
+        if p and m:
+            pass
+        else:
+            return HttpResponse("please use exist professor_id or module_code")
+        for item in m:
+            print(item)
+            ratings = list(models.Rating.objects.filter(professer_id=p[0].id,module_id=item.id))
+            for item in ratings:
+                total_star += int(item.stars)
+                number += 1
+        print(total_star)
+        print(number)
+        avg_star = str(int(Decimal(total_star /number ).quantize(Decimal('1'), rounding=ROUND_HALF_UP)) * '*')
+        out_list = list()
+        out_list.append("The rating of Professor " + p[0].first_name + "." + p[0].last_name + " (" + p[
+            0].professor_id + ") in " + m[0].name + "(" + module_code + ") " + " is:" + avg_star)
+        return HttpResponse(out_list)
     except:
-        return HttpResponse("please make sure professor_id and module_code are correct")
-    total_star = 0
-    number = 0;
+        return HttpResponse("the professor's module which you selected has no rating now ")
+
     # p_id = 0;
     out_list = list()
     for idx,item in enumerate(ratings):
         total_star += int(item.stars)
         number = idx + 1
     avg_star = str(int(Decimal(total_star / number).quantize(Decimal('1'), rounding=ROUND_HALF_UP)) * '*')
-    out_list.append("The rating of Professor " + p[0].first_name + "." + p[0].last_name + " (" + p[
-        0].professor_id + ") in "+ m[0].name+"("+m[0].code+") "+" is:" + avg_star)
-    return HttpResponse(out_list)
+
+@csrf_exempt
 def login(request):
-    username = request.GET.get('userName',None)
-    password = request.GET.get('password',None)
+    username = request.POST.get('userName',None)
+    password = request.POST.get('password',None)
+    print(username)
     if username and password:
         username = username.strip()
         try:
@@ -97,11 +141,20 @@ def login(request):
     return HttpResponse("please make sure your input params are not null")
 def loginOut(request):
     user_list = list(models.Users.objects.all())
+    active_list = list()
+    for item in user_list:
+        if item.is_active == '1':
+            active_list.append(item.userName)
+            active_list.append('1')
+    if '1' not in active_list:
+        return HttpResponse("please login or register first")
+    user_list = list(models.Users.objects.all())
     for item in user_list:
         models.Users.objects.filter(userName=item.userName).update(is_active='0')
     return HttpResponse("login out success")
+@csrf_exempt
 def register(request):
-    username = request.GET.get("userName",None)
+    username = request.POST.get("userName",None)
     if username:
         try:
             user = models.Users.objects.get(userName=username)
@@ -111,17 +164,16 @@ def register(request):
             return  HttpResponse("this user name has registered,please use another one")
     else:
         return HttpResponse("please input username")
-    password = request.GET.get("password",None)
-    email = request.GET.get("email",None)
+    password = request.POST.get("password",None)
+    email = request.POST.get("email",None)
     if password and email:
         try:
-            u = models.Users(userName=username,password=password,email=email)
-            u.save()
+            u = models.Users.objects.create(userName=username,password=password,email=email)
             return HttpResponse("register success")
         except:
             return HttpResponse("insert wrong")
     return HttpResponse("please input password or email")
-
+@csrf_exempt
 def rate(request):
     user_list = list(models.Users.objects.all())
     active_list = list()
@@ -131,22 +183,21 @@ def rate(request):
             active_list.append('1')
     if '1' not in active_list:
         return HttpResponse("please login or register first")
-    professor_id = request.GET.get("professor_id",None)
-    module_code = request.GET.get("module_code",None)
-    year = request.GET.get("year", None)
-    semester = request.GET.get("semester", None)
-    rating = request.GET.get("rating", None)
-    print(professor_id,module_code,year,semester,rating)
+    professor_id = request.POST.get("professor_id",None)
+    module_code = request.POST.get("module_code",None)
+    year = request.POST.get("year", None)
+    semester = request.POST.get("semester", None)
+    rating = request.POST.get("rating", None)
     if professor_id ==None or module_code==None or year==None or semester==None or rating ==None:
         return HttpResponse("please make sure all input params are not null")
     try:
         stars = int(rating)
-        print(stars)
         if stars <1 or stars>5:
             return HttpResponse("please input a int param which is in range of 1 to 5")
     except:
         return HttpResponse("please input a int param")
     try:
+        print(professor_id)
         p = models.Professors.objects.get(professor_id=professor_id)
     except models.Professors.DoesNotExist:
         return HttpResponse("the professor is not in the school list yet")
@@ -154,8 +205,7 @@ def rate(request):
         m =models.Moudles.objects.get(code=module_code,year=year,semester=semester)
     except models.Moudles.DoesNotExist:
         return HttpResponse("the module is not in the school's moudle list")
-    r = models.Rating(professer=p,stars=stars,module=m)
-    r.save()
+    r = models.Rating.objects.create(professer=p,stars=stars,module=m)
     return HttpResponse("rating success")
 
 def showRatings4MoudlesYearSemester(request):
